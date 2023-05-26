ae657d927309dfedd757e66d38aae65faf937646
Eko P <abumkha@gmail.com>
2023-05-26 10:41:42 +0700

    Example

--- src/main/java/eco/bookpickup/PickUpRequest.java
+++ src/main/java/eco/bookpickup/PickUpRequest.java
@@ -1,12 +1,14 @@
 package eco.bookpickup;
 
 import eco.bookpickup.model.PickUp;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 
 @Data
 public class PickUpRequest extends PickUp {
 
+    @Schema(example = "/works/OL21177W")
     String bookKey;
 
 }
